f77974b51f90ab7b9f3f999096aea11c461fca2a
Fuad Agil <far1023@users.noreply.github.com>
2023-03-27 15:29:18 +0700

    add role seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +17,7 @@ class DatabaseSeeder extends Seeder
 	{
 		$this->call([
 			SuperadminSeeder::class,
+			RoleSeeder::class,
 		]);
 	}
 }
