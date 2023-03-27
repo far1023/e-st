b20f1e3def451c5fc3a16dc4b22a9e1670120f86
Fuad Agil <far1023@users.noreply.github.com>
2023-03-27 15:28:05 +0700

    add user CRUD

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,240 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
+use Spatie\Permission\Models\Role;
+use App\Http\Resources\APIResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+	public function index()
+	{
+		return view('back/content/user', [
+			// "roles" => Role::all()->where('id', '!=', 1),
+			"roles" => Role::all(),
+			"title" => "Data Pengguna Sistem",
+			"css"	=> ['datatable'],
+			"js"	=> 'userjs',
+		]);
+	}
+
+	public function dttable()
+	{
+		$user = User::find(Auth::user()->id);
+
+		// $data = User::with('roles')->where('id', '!=', 1);
+		$data = User::with('roles');
+
+		return DataTables::of($data->get())
+			->addColumn('aksi', function ($data) use ($user) {
+				$aksi = "";
+
+				if ($user->can('edit skt')) {
+					$aksi .= "<a href='javascript:void(0)' data-id='" . $data['id'] . "' class='btn btn-sm btn-secondary mb-1 mx-1 edit'>Edit</a>";
+				}
+				if ($user->can('delete skt')) {
+					$aksi .= " <a href='javascript:void(0)' data-id='" . $data['id'] . "' class='btn btn-sm btn-danger mb-1 mx-1 hapus'>Delete</a>";
+				}
+
+				return $aksi;
+			})
+			->addColumn('level', function ($s) {
+				$has_roles = [];
+
+				foreach ($s->roles as $val) {
+					$has_roles[] = $val->name;
+				}
+				return implode(', ', $has_roles);
+			})
+			->rawColumns(['aksi', 'level'])
+			->addIndexColumn()
+			->toJson();
+	}
+
+	public function show(int $id)
+	{
+		try {
+			$data = User::with('roles')->find($id);
+			return response()->json(
+				new APIResponse(
+					true,
+					"Data pengguna ditemukan",
+					$data
+				),
+				201
+			);
+		} catch (\Throwable $th) {
+			return response()->json(
+				new APIResponse(
+					false,
+					$th->getMessage()
+				),
+				500
+			);
+		}
+	}
+
+	public function store(Request $request)
+	{
+		$validator = Validator::make(
+			$request->all(),
+			[
+				"name" => ["required"],
+				"username" => ["required", "unique:users,username"],
+				"password" => ["nullable", "confirmed"],
+				"id_role" => ["required"],
+			],
+			[
+				"name.required" => "wajib diisi",
+				"username.required" => "wajib diisi",
+				"username.unique" => "username sudah terdaftar",
+				"password.confirmed" => "password tidak cocok",
+				"id_role.required" => "wajib diisi",
+			]
+		);
+
+		if ($validator->fails()) {
+			return response()->json(
+				new APIResponse(
+					false,
+					"Invalid input",
+					$validator->errors()->toArray()
+				),
+				422
+			);
+		}
+
+		$data = array(
+			'name' => $request->name,
+			'username' => $request->username,
+			'password' => Hash::make($request->password),
+		);
+
+		try {
+			$user = User::create($data);
+			$user->syncRoles($request->id_role);
+			return response()->json(
+				new APIResponse(
+					true,
+					"Data pengguna berhasil disimpan",
+					$request->all()
+				),
+				201
+			);
+		} catch (\Throwable $th) {
+			return response()->json(
+				new APIResponse(
+					false,
+					$th->getMessage()
+				),
+				500
+			);
+		}
+	}
+
+	public function update(Request $request, int $id)
+	{
+		$validator = Validator::make(
+			$request->all(),
+			[
+				"name" => ["required"],
+				"username" => ["required", "unique:users,username," . $id],
+				"password" => ["nullable", "confirmed"],
+				"id_role" => ["required"],
+			],
+			[
+				"name.required" => "wajib diisi",
+				"username.required" => "wajib diisi",
+				"username.unique" => "username sudah terdaftar",
+				"password.confirmed" => "password tidak cocok",
+				"id_role.required" => "wajib diisi",
+			]
+		);
+
+		if ($validator->fails()) {
+			return response()->json(
+				new APIResponse(
+					false,
+					"Invalid input",
+					$validator->errors()->toArray()
+				),
+				422
+			);
+		}
+
+		// var_dump($request->all());
+		// die;
+
+		$data = array(
+			'name' => $request->name,
+			'username' => $request->username
+		);
+
+		if ($request->password) {
+			$data['password'] = Hash::make($request->password);
+		}
+
+		try {
+			$user = User::findOrFail($id);
+			$user->update($data);
+			$user->syncRoles($request->id_role);
+			return response()->json(
+				new APIResponse(
+					true,
+					"Data pengguna berhasil diperbaharui",
+					$request->all()
+				),
+				201
+			);
+		} catch (\Throwable $th) {
+			return response()->json(
+				new APIResponse(
+					false,
+					$th->getMessage()
+				),
+				500
+			);
+		}
+	}
+
+	public function destroy(int $id)
+	{
+		$user = User::find(Auth::user()->id);
+		$data = User::findOrFail($id);
+
+		if (!$user->can('delete user')) {
+			return response()->json(
+				new APIResponse(
+					false,
+					"access to the requested resource is forbidden"
+				),
+				403
+			);
+		}
+
+		try {
+			$data->delete();
+			return response()->json(
+				new APIResponse(
+					true,
+					"Data pengguna telah dihapus",
+				),
+				200
+			);
+		} catch (\Throwable $th) {
+			return response()->json(
+				new APIResponse(
+					false,
+					$th->getMessage()
+				),
+				500
+			);
+		}
+	}
+}
---
+++ resources/views/back/content/user.blade.php
@@ -0,0 +1,89 @@
+@extends('back.layouts.main')
+
+@section('content')
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">{{ $title }}</h1>
+        </div>
+        <div class="col-sm-6">
+          <button type="button" id="add" class="btn btn-sm btn-outline-primary float-sm-right">Tambah pengguna</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="content">
+    <div class="container-fluid">
+      <div class="card">
+        <div class="card-body">
+          <div class="table-responsive py-3">
+            <table class="table table-striped table-hover dttables" id="dt_user">
+              <thead>
+                <tr>
+                  <th style="text-align: center;">#</th>
+                  <th style="text-align: center;">Username</th>
+                  <th style="text-align: center;">Nama</th>
+                  <th style="text-align: center;">Level</th>
+                  <th style="text-align: center;">Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal fade" id="modal_pengguna" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="modal_title"></h5>
+        </div>
+        <form id="form_pengguna" name="form_pengguna">
+          @csrf
+          <div class="modal-body">
+            <input type="hidden" class="d-none" name="id" id="id" value="">
+            <div class="form-group">
+              <label for="name">Nama Lengkap</label>
+              <input type="text" class="form-control" id="name" name="name">
+              <small class="text-danger err-msg" id="name_error"></small>
+            </div>
+            <div class="form-group">
+              <label for="id_role">Level</label>
+              <select class="form-control" name="id_role" id="id_role">
+                <option value="-" disabled selected>--pilih</option>
+                @foreach ($roles as $item)
+                  <option value="{{ $item->id }}">{{ $item->name }}</option>
+                @endforeach
+              </select>
+              <small class="text-danger err-msg" id="name_error"></small>
+            </div>
+            <div class="form-group">
+              <label for="username">Username</label>
+              <input type="text" class="form-control" id="username" name="username">
+              <small class="text-danger err-msg" id="username_error"></small>
+            </div>
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input type="password" class="form-control" id="password" name="password">
+              <small class="text-danger err-msg" id="password_error"></small>
+            </div>
+            <div class="form-group">
+              <label for="name">Ulangi Password</label>
+              <input type="password" class="form-control" id="password_confirmation" name="password_confirmation">
+              <small class="text-danger err-msg" id="password_confirmation_error"></small>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="submit" class="btn btn-primary px-4" id="btn-save">Simpan</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+@endsection
