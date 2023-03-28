763a9c845f8736b41eb747351473fe67405cadaa
Fuad Agil <far1023@users.noreply.github.com>
2023-03-29 00:32:37 +0700

    add Peta Situasi CRUD

---
+++ app/Http/Controllers/PetaSituasiTanahController.php
@@ -0,0 +1,296 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use VignereCip;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Models\PetaSituasiTanah;
+use Yajra\DataTables\DataTables;
+use App\Http\Resources\APIResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class PetaSituasiTanahController extends Controller
+{
+	public function index()
+	{
+	}
+
+	public function data()
+	{
+		return view('back.content.data.petaSituasi', [
+			"title" => "Data Peta Situasi Tanah",
+			"css"	=> ['datatable'],
+			"js"	=> 'data/petaSituasiJs'
+		]);
+	}
+
+	public function dttable()
+	{
+		$user = User::find(Auth::user()->id);
+
+		$data = PetaSituasiTanah::latest()->get()->toArray();
+
+		foreach ($data as $i => $value) {
+			foreach ($value as $j => $val) {
+				if (!is_int($val)) {
+					$value[$j] = VignereCip::decrypt($val);
+				}
+			}
+			$data[$i] = $value;
+		}
+
+		return DataTables::of($data)
+			->addColumn('aksi', function ($data) use ($user) {
+				$aksi = "";
+
+				if ($user->can('edit peta-situasi')) {
+					$aksi .= "<a href='" . url('formulir/peta-situasi/' . $data['id'] . '/edit') . "' class='btn btn-sm btn-secondary mb-1 mx-1 edit'>Edit</a>";
+				}
+				if ($user->can('delete peta-situasi')) {
+					$aksi .= " <a href='javascript:void(0)' data-id='" . $data['id'] . "' class='btn btn-sm btn-danger mb-1 mx-1 hapus'>Delete</a>";
+				}
+
+				return $aksi;
+			})
+			->rawColumns(['aksi'])
+			->addIndexColumn()
+			->toJson();
+	}
+
+	public function show(int $id)
+	{
+		try {
+			$data = PetaSituasiTanah::findOrFail($id);
+			return response()->json(
+				new APIResponse(
+					true,
+					"Data Peta Situasi Tanah ditemukan",
+					$data
+				),
+				201
+			);
+		} catch (\Throwable $th) {
+			return response()->json(
+				new APIResponse(
+					false,
+					$th->getMessage()
+				),
+				500
+			);
+		}
+	}
+
+	public function create()
+	{
+		$kades = User::whereHas('roles', function ($query) {
+			$query->where('name', 'kades');
+		})->first();
+
+		$data = [
+			"kades" => ($kades) ? $kades->name : ''
+		];
+
+		return view('back.content.formulir.formPetaSituasi', [
+			"data" => $data,
+			"title" => "Formulir Peta Situasi Tanah",
+			"css"	=> ['stepper', 'datepicker'],
+			"js"	=> 'formulir/addPetaSituasiJs'
+		]);
+	}
+
+	public function store(Request $request)
+	{
+		$validator = Validator::make(
+			$request->all(),
+			[
+				"jalan_gang" => ["required"],
+				"rt" => ["required"],
+				"rw" => ["required"],
+				"desa" => ["required"],
+				"kecamatan" => ["required"],
+				"kabupaten" => ["required"],
+				"luas_tanah" => ["required", "integer"],
+				"atas_nama" => ["required"],
+				"sketsa" => ["nullable", "mimes:jpg,jpeg,png", "max:2048"],
+				"jabatan_saksi_satu" => ["required_with:nama_saksi_satu"],
+				"jabatan_saksi_dua" => ["required_with:nama_saksi_dua"],
+				"jabatan_saksi_tiga" => ["required_with:nama_saksi_tiga"],
+				"mengetahui" => ["required"],
+			],
+			[
+				"jalan_gang.required" => "wajib diisi",
+				"rt.required" => "wajib diisi",
+				"rw.required" => "wajib diisi",
+				"desa.required" => "wajib diisi",
+				"kecamatan.required" => "wajib diisi",
+				"kabupaten.required" => "wajib diisi",
+				"luas_tanah.required" => "wajib diisi",
+				"luas_tanah.integer" => "isikan hanya dengan bilangan bulat",
+				"atas_nama" => "wajib diisi",
+				"sketsa.mimes" => "jenis file yang diterima hanya jpg/jpeg/png",
+				"sketsa.max" => "ukuran file maksimal 2MB",
+				"jabatan_saksi_satu.required_with" => "isikan jabatan saksi satu",
+				"jabatan_saksi_dua.required_with" => "isikan jabatan saksi dua",
+				"jabatan_saksi_tiga.required_with" => "isikan jabatan saksi tiga",
+				"mengetahui.required" => "wajib diisi",
+			]
+		);
+
+		if ($validator->fails()) {
+			return response()->json(
+				new APIResponse(
+					false,
+					"Invalid input",
+					$validator->errors()->toArray()
+				),
+				422
+			);
+		}
+
+		foreach ($request->all() as $key => $value) {
+			$request[$key] = VignereCip::encrypt($value);
+		}
+
+		try {
+			PetaSituasiTanah::create($request->all());
+			return response()->json(
+				new APIResponse(
+					true,
+					"Data Peta Situasi Tanah berhasil disimpan",
+					$request->all()
+				),
+				201
+			);
+		} catch (\Throwable $th) {
+			return response()->json(
+				new APIResponse(
+					false,
+					$th->getMessage()
+				),
+				500
+			);
+		}
+	}
+
+	public function edit()
+	{
+		return view('back.content.formulir.formPetaSituasi', [
+			"data" => NULL,
+			"title" => "Formulir Peta Situasi Tanah - Edit",
+			"css"	=> ['stepper', 'datepicker'],
+			"js"	=> 'formulir/editPetaSituasiJs'
+		]);
+	}
+
+	public function update(Request $request, int $id)
+	{
+		$validator = Validator::make(
+			$request->all(),
+			[
+				"jalan_gang" => ["required"],
+				"rt" => ["required"],
+				"rw" => ["required"],
+				"desa" => ["required"],
+				"kecamatan" => ["required"],
+				"kabupaten" => ["required"],
+				"luas_tanah" => ["required", "integer"],
+				"atas_nama" => ["required"],
+				"sketsa" => ["nullable", "mimes:jpg,jpeg,png", "max:2048"],
+				"jabatan_saksi_satu" => ["required_with:nama_saksi_satu"],
+				"jabatan_saksi_dua" => ["required_with:nama_saksi_dua"],
+				"jabatan_saksi_tiga" => ["required_with:nama_saksi_tiga"],
+				"mengetahui" => ["required"],
+			],
+			[
+				"jalan_gang.required" => "wajib diisi",
+				"rt.required" => "wajib diisi",
+				"rw.required" => "wajib diisi",
+				"desa.required" => "wajib diisi",
+				"kecamatan.required" => "wajib diisi",
+				"kabupaten.required" => "wajib diisi",
+				"luas_tanah.required" => "wajib diisi",
+				"luas_tanah.integer" => "isikan hanya dengan bilangan bulat",
+				"atas_nama" => "wajib diisi",
+				"sketsa.mimes" => "jenis file yang diterima hanya jpg/jpeg/png",
+				"sketsa.max" => "ukuran file maksimal 2MB",
+				"jabatan_saksi_satu.required_with" => "isikan jabatan saksi satu",
+				"jabatan_saksi_dua.required_with" => "isikan jabatan saksi dua",
+				"jabatan_saksi_tiga.required_with" => "isikan jabatan saksi tiga",
+				"mengetahui.required" => "wajib diisi",
+			]
+		);
+
+		if ($validator->fails()) {
+			return response()->json(
+				new APIResponse(
+					false,
+					"Invalid input",
+					$validator->errors()->toArray()
+				),
+				422
+			);
+		}
+
+		foreach ($request->all() as $key => $value) {
+			$request[$key] = VignereCip::encrypt($value);
+		}
+
+		try {
+			$data = PetaSituasiTanah::findOrFail($id);
+			$data->update($request->all());
+			return response()->json(
+				new APIResponse(
+					true,
+					"Data Peta Situasi Tanah berhasil diperbaharui",
+					$request->all()
+				),
+				201
+			);
+		} catch (\Throwable $th) {
+			return response()->json(
+				new APIResponse(
+					false,
+					$th->getMessage()
+				),
+				500
+			);
+		}
+	}
+
+	public function destroy(int $id)
+	{
+		$user = User::find(Auth::user()->id);
+		$data = PetaSituasiTanah::findOrFail($id);
+
+		if (!$user->can('delete peta-situasi')) {
+			return response()->json(
+				new APIResponse(
+					false,
+					"access to the requested resource is forbidden"
+				),
+				403
+			);
+		}
+
+		try {
+			$data->delete();
+			return response()->json(
+				new APIResponse(
+					true,
+					"Data Peta Situasi Tanah telah dihapus",
+				),
+				200
+			);
+		} catch (\Throwable $th) {
+			return response()->json(
+				new APIResponse(
+					false,
+					$th->getMessage()
+				),
+				500
+			);
+		}
+	}
+}
---
+++ resources/views/back/content/formulir/formPetaSituasi.blade.php
@@ -0,0 +1,188 @@
+@extends('back.layouts.main')
+
+@section('content')
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">{{ $title }}</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="content">
+    <div class="container-fluid">
+      <div class="card">
+        <div class="card-header text-center">Sceets Kaartsceests Kaart</div>
+        <div class="card-body">
+          <form id="formpetasituasi" class="px-lg-5">
+            @csrf
+            {{ request()->route('id') ? method_field('PUT') : '' }}
+            <div class="bs-stepper">
+              <div class="bs-stepper-header d-none" role="tablist">
+                <div class="step" data-target="#data-tanah">
+                  <button type="button" class="step-trigger" role="tab" aria-controls="data-tanah" id="data-tanah-trigger">
+                    <span class="bs-stepper-circle">1</span>
+                    <span class="bs-stepper-label">Pihak Pertama</span>
+                  </button>
+                </div>
+                <div class="line"></div>
+                <div class="step" data-target="#diketahui">
+                  <button type="button" class="step-trigger" role="tab" aria-controls="diketahui" id="diketahui-trigger">
+                    <span class="bs-stepper-circle">4</span>
+                    <span class="bs-stepper-label">Yang Mengetahui</span>
+                  </button>
+                </div>
+              </div>
+              <div class="bs-stepper-content pt-4">
+                <div id="data-tanah" class="content" role="tabpanel" aria-labelledby="data-tanah-trigger">
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label for="jalan_gang">Jalan / Gang</label>
+                        <input type="text" class="form-control" name="jalan_gang" id="jalan_gang" placeholder="">
+                        <small class="text-danger err-msg" id="jalan_gang_error"></small>
+                      </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-group">
+                        <label for="rt">RT</label>
+                        <input type="text" class="form-control" name="rt" id="rt" oninput="this.value=this.value.replace(/[^\d]/,'')">
+                        <small class="text-danger err-msg" id="rt_error"></small>
+                      </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-group">
+                        <label for="rw">RW</label>
+                        <input type="text" class="form-control" name="rw" id="rw" oninput="this.value=this.value.replace(/[^\d]/,'')">
+                        <small class="text-danger err-msg" id="rw_error"></small>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label for="dusun">Nama Dusun</label>
+                        <input type="text" class="form-control" name="dusun" id="dusun" placeholder="">
+                        <small class="text-danger err-msg" id="dusun_error"></small>
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label for="desa">Nama Desa</label>
+                        <input type="text" class="form-control" name="desa" id="desa" placeholder="">
+                        <small class="text-danger err-msg" id="desa_error"></small>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label for="kecamatan">Kecamatan</label>
+                        <input type="text" class="form-control" name="kecamatan" id="kecamatan" placeholder="">
+                        <small class="text-danger err-msg" id="kecamatan_error"></small>
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label for="kabupaten">Kabupaten</label>
+                        <input type="text" class="form-control" name="kabupaten" id="kabupaten" placeholder="">
+                        <small class="text-danger err-msg" id="kabupaten_error"></small>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="luas_tanah">Luas Tanah</label>
+                    <div class="input-group">
+                      <input type="number" min="1" class="form-control" name="luas_tanah" id="luas_tanah">
+                      <div class="input-group-append">
+                        <span class="input-group-text">m&#178;</span>
+                      </div>
+                    </div>
+                    <small class="text-danger err-msg" id="luas_tanah_error"></small>
+                  </div>
+                  <div class="form-group">
+                    <label for="atas_nama">Atas Nama</label> <small><i>(yang membuat pernyataan)</i></small>
+                    <input type="text" class="form-control" name="atas_nama" id="atas_nama" placeholder="">
+                    <small class="text-danger err-msg" id="atas_nama_error"></small>
+                  </div>
+                  <div class="form-group">
+                    <label for="sketsa">Sketsa</label>
+                    <input type="text" class="form-control" name="sketsa" id="sketsa" placeholder="">
+                    <small class="text-danger err-msg" id="sketsa_error"></small>
+                  </div>
+                  <div class="pt-4">
+                    <button type="button" class="btn btn-outline-secondary" onclick="stepper.previous()"><i class="las la-chevron-left"></i> Sebelumnya</button>
+                    <button type="button" class="btn btn-outline-primary float-right mb-3" onclick="stepper.next()">Selanjutnya <i class="las la-chevron-right"></i></button>
+                  </div>
+                </div>
+                <div id="diketahui" class="content" role="tabpanel" aria-labelledby="diketahui-trigger">
+                  <p>Yang menjadi saksi :</p>
+                  <div class="row">
+                    <div class="col-sm-6">
+                      <div class="form-group">
+                        <label for="nama_saksi_satu">Nama Saksi Satu</label>
+                        <input type="text" class="form-control" name="nama_saksi_satu" id="nama_saksi_satu" placeholder="">
+                        <small class="text-danger err-msg" id="nama_saksi_satu_error"></small>
+                      </div>
+                    </div>
+                    <div class="col-sm-6">
+                      <div class="form-group">
+                        <label for="jabatan_saksi_satu">Jabatan Saksi Satu</label>
+                        <input type="text" class="form-control" name="jabatan_saksi_satu" id="jabatan_saksi_satu" placeholder="">
+                        <small class="text-danger err-msg" id="jabatan_saksi_satu_error"></small>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-sm-6">
+                      <div class="form-group">
+                        <label for="nama_saksi_dua">Nama Saksi Dua</label>
+                        <input type="text" class="form-control" name="nama_saksi_dua" id="nama_saksi_dua" placeholder="">
+                        <small class="text-danger err-msg" id="nama_saksi_dua_error"></small>
+                      </div>
+                    </div>
+                    <div class="col-sm-6">
+                      <div class="form-group">
+                        <label for="jabatan_saksi_dua">Jabatan Saksi Dua</label>
+                        <input type="text" class="form-control" name="jabatan_saksi_dua" id="jabatan_saksi_dua" placeholder="">
+                        <small class="text-danger err-msg" id="jabatan_saksi_dua_error"></small>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-sm-6">
+                      <div class="form-group">
+                        <label for="nama_saksi_tiga">Nama Saksi Tiga</label>
+                        <input type="text" class="form-control" name="nama_saksi_tiga" id="nama_saksi_tiga" placeholder="">
+                        <small class="text-danger err-msg" id="nama_saksi_tiga_error"></small>
+                      </div>
+                    </div>
+                    <div class="col-sm-6">
+                      <div class="form-group">
+                        <label for="jabatan_saksi_tiga">Jabatan Saksi Tiga</label>
+                        <input type="text" class="form-control" name="jabatan_saksi_tiga" id="jabatan_saksi_tiga" placeholder="">
+                        <small class="text-danger err-msg" id="jabatan_saksi_tiga_error"></small>
+                      </div>
+                    </div>
+                  </div>
+                  <p class="pt-4">Yang mengetahui :</p>
+                  <div class="form-group">
+                    <label for="mengetahui">Kepala Desa Air Putih</label>
+                    <input type="text" class="form-control" name="mengetahui" id="mengetahui" value="{{ $data ? $data['kades'] : '' }}">
+                    <small class="text-danger err-msg" id="mengetahui_error"></small>
+                  </div>
+                  <div class="pt-4">
+                    <button type="button" class="btn btn-outline-secondary" onclick="stepper.previous()"><i class="las la-chevron-left"></i> Sebelumnya</button>
+                    <button type="submit" class="btn btn-primary float-right px-4" id="btn-save">Simpan data</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
