3c92ec7781bf292abf66e9dad6318d5585667b40
Fuad Agil <far1023@users.noreply.github.com>
2023-03-29 13:44:55 +0700

    update sidebar

--- resources/views/back/layouts/partials/sidebar.blade.php
+++ resources/views/back/layouts/partials/sidebar.blade.php
@@ -6,8 +6,9 @@
   <div class="sidebar">
     <nav class="mt-2">
       <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <li class="nav-header">NAVIGASI UTAMA</li>
         <li class="nav-item">
-          <a href="{{ url('beranda') }}" class="nav-link active">
+          <a href="{{ url('beranda') }}" class="nav-link {{ Request::segment(1) == 'beranda' ? 'active' : '' }}">
             <i class="nav-icon las la-th"></i>
             <p>
               Beranda
@@ -15,15 +16,15 @@
           </a>
         </li>
         <li class="nav-item">
-          <a href="{{ url('pengguna') }}" class="nav-link">
+          <a href="{{ url('pengguna') }}" class="nav-link {{ Request::segment(1) == 'pengguna' ? 'active' : '' }}">
             <i class="nav-icon las la-th"></i>
             <p>
               Pengguna
             </p>
           </a>
         </li>
-        <li class="nav-item">
-          <a href="javascript:void(0)" class="nav-link">
+        <li class="nav-item {{ Request::segment(1) == 'formulir' ? 'menu-open' : '' }}">
+          <a href="javascript:void(0)" class="nav-link {{ Request::segment(1) == 'formulir' ? 'active' : '' }}">
             <i class="nav-icon las la-tachometer-alt"></i>
             <p>
               Form Surat
@@ -32,33 +33,29 @@
           </a>
           <ul class="nav nav-treeview">
             <li class="nav-item">
-              <a href="{{ url('formulir/ganti-rugi') }}" class="nav-link">
-                <i class="las la-circle nav-icon"></i>
-                <p>SPGR</p>
+              <a href="{{ url('formulir/ganti-rugi') }}" class="nav-link {{ Request::segment(2) == 'ganti-rugi' ? 'active' : '' }}">
+                &emsp;&emsp;<p>SPGR</p>
               </a>
             </li>
             <li class="nav-item">
-              <a href="{{ url('formulir/kepemilikan-tanah') }}" class="nav-link">
-                <i class="las la-circle nav-icon"></i>
-                <p>SKT</p>
+              <a href="{{ url('formulir/kepemilikan-tanah') }}" class="nav-link {{ Request::segment(2) == 'kepemilikan-tanah' ? 'active' : '' }}">
+                &emsp;&emsp;<p>SKT</p>
               </a>
             </li>
             <li class="nav-item">
-              <a href="{{ url('formulir/peta-situasi-tanah') }}" class="nav-link">
-                <i class="las la-circle nav-icon"></i>
-                <p>Peta Situasi Tanah</p>
+              <a href="{{ url('formulir/peta-situasi-tanah') }}" class="nav-link {{ Request::segment(2) == 'peta-situasi-tanah' ? 'active' : '' }}">
+                &emsp;&emsp;<p>Peta Situasi Tanah</p>
               </a>
             </li>
             <li class="nav-item">
-              <a href="{{ url('formulir/surat-situasi-tanah') }}" class="nav-link">
-                <i class="las la-circle nav-icon"></i>
-                <p>Surat Situasi Tanah</p>
+              <a href="{{ url('formulir/surat-situasi-tanah') }}" class="nav-link {{ Request::segment(2) == 'surat-situasi-tanah' ? 'active' : '' }}">
+                &emsp;&emsp;<p>Surat Situasi Tanah</p>
               </a>
             </li>
           </ul>
         </li>
-        <li class="nav-item">
-          <a href="javascript:void(0)" class="nav-link">
+        <li class="nav-item {{ Request::segment(1) == 'data' ? 'menu-open' : '' }}">
+          <a href="javascript:void(0)" class="nav-link {{ Request::segment(1) == 'data' ? 'active' : '' }}">
             <i class="nav-icon las la-tachometer-alt"></i>
             <p>
               Data Surat
@@ -67,31 +64,56 @@
           </a>
           <ul class="nav nav-treeview">
             <li class="nav-item">
-              <a href="{{ url('data/ganti-rugi') }}" class="nav-link">
-                <i class="las la-circle nav-icon"></i>
-                <p>SPGR</p>
+              <a href="{{ url('data/ganti-rugi') }}" class="nav-link {{ Request::segment(2) == 'ganti-rugi' ? 'active' : '' }}">
+                &emsp;&emsp;<p>SPGR</p>
               </a>
             </li>
             <li class="nav-item">
-              <a href="{{ url('data/kepemilikan-tanah') }}" class="nav-link">
-                <i class="las la-circle nav-icon"></i>
-                <p>SKT</p>
+              <a href="{{ url('data/kepemilikan-tanah') }}" class="nav-link {{ Request::segment(2) == 'kepemilikan-tanah' ? 'active' : '' }}">
+                &emsp;&emsp;<p>SKT</p>
               </a>
             </li>
             <li class="nav-item">
-              <a href="{{ url('data/peta-situasi-tanah') }}" class="nav-link">
-                <i class="las la-circle nav-icon"></i>
-                <p>Peta Situasi Tanah</p>
+              <a href="{{ url('data/peta-situasi-tanah') }}" class="nav-link {{ Request::segment(2) == 'peta-situasi-tanah' ? 'active' : '' }}">
+                &emsp;&emsp;<p>Peta Situasi Tanah</p>
               </a>
             </li>
             <li class="nav-item">
-              <a href="{{ url('data/surat-situasi-tanah') }}" class="nav-link">
-                <i class="las la-circle nav-icon"></i>
-                <p>Surat Situasi Tanah</p>
+              <a href="{{ url('data/surat-situasi-tanah') }}" class="nav-link {{ Request::segment(2) == 'surat-situasi-tanah' ? 'active' : '' }}">
+                &emsp;&emsp;<p>Surat Situasi Tanah</p>
               </a>
             </li>
           </ul>
         </li>
+        @can('access control-room')
+          <li class="nav-header">CONTROL ROOM</li>
+          <li class="nav-item {{ Request::segment(1) == 'controls' ? 'menu-open' : '' }}">
+            <a href="javascript:void(0)" class="nav-link {{ Request::segment(1) == 'controls' ? 'active' : '' }}">
+              <i class="nav-icon las la-tachometer-alt"></i>
+              <p>
+                ACCESS
+                <i class="right las la-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="{{ url('controls/roles') }}" class="nav-link {{ Request::segment(2) == 'roles' ? 'active' : '' }}">
+                  &emsp;&emsp;<p>Roles</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ url('controls/permissions') }}" class="nav-link {{ Request::segment(2) == 'permissions' ? 'active' : '' }}">
+                  &emsp;&emsp;<p>Permissions</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ url('controls/access-granting') }}" class="nav-link {{ Request::segment(2) == 'access-granting' ? 'active' : '' }}">
+                  &emsp;&emsp;<p>Access Granting</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+        @endcan
       </ul>
     </nav>
   </div>
