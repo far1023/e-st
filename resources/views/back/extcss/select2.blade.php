88f47b4edeb7d3c2934fcb94fe95976f9c0ff54c
Fuad Agil <far1023@users.noreply.github.com>
2023-03-30 14:19:19 +0700

    add spatie permission into route

---
+++ resources/views/back/extcss/select2.blade.php
@@ -0,0 +1,2 @@
+<link href="{{ asset('template/plugins/select2/css/select2.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('template/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
